3fc1eecea4a4e5e95e544671eac9b76d1d56d618
alies7790 <ali.e7790@gmail.com>
2021-11-06 14:18:08 +0330

    change procfile for channel

--- smart_video_door_phone/settings.py
+++ smart_video_door_phone/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lr+y5t8s$v-lv1nw0qoyidew-48ted64pyndgv*tvij_823&44'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','smartvideodoorphoneproject.herokuapp.com']
 
