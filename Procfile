5336cfb4e1d1f52c9c638af5e62a3c1745638b68
alies7790 <ali.e7790@gmail.com>
2021-11-15 01:02:43 +0330

    celery setting change

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 release: python manage.py migrate
-web: gunicorn smart_video_door_phone.wsgi --log-file -
+web: gunicorn daphne smart_video_door_phone.asgi:application --port $PORT --bind 0.0.0.0 -v2
 daphne: daphne smart_video_door_phone.asgi:application --port $PORT --bind 0.0.0.0 -v2
 celery: celery -A smart_video_door_phone.celery worker -l info
 celerybeat: celery -A smart_video_door_phone beat -l INFO
--- smart_video_door_phone/settings.py
+++ smart_video_door_phone/settings.py
@@ -202,9 +202,11 @@ django_heroku.settings(locals())
 # CELERY SETTINGS
 CELERY_BROKER_URL = 'redis://:as8qO8oBJBLiOCVNuCr52YNLKNHttUah@redis-11841.c251.east-us-mz.azure.cloud.redislabs.com:11841/'
 #CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
-
-
+CELERY_RESULT_BACKEND = 'redis://:as8qO8oBJBLiOCVNuCr52YNLKNHttUah@redis-11841.c251.east-us-mz.azure.cloud.redislabs.com:11841/'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SELERLIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Tehran'
 
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
