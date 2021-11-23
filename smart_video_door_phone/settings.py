6d031402ea0f7b9c8ec78ce14cf7883d4176e36b
alies7790 <ali.e7790@gmail.com>
2021-11-23 19:35:30 +0330

    celery setting change

--- smart_video_door_phone/settings.py
+++ smart_video_door_phone/settings.py
@@ -116,7 +116,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [{'address':("redis-11841.c251.east-us-mz.azure.cloud.redislabs.com", 11841),'password':"as8qO8oBJBLiOCVNuCr52YNLKNHttUah",}]
+            "hosts": [{'address':("redis-17762.c282.east-us-mz.azure.cloud.redislabs.com", 17762),'password':"VtAcXvcsq1m7LT9rbyDj2NOBdx90naBm",}]
         },
     },
 }
@@ -202,7 +202,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # django_heroku.settings(locals())
 
 # CELERY SETTINGS
-CELERY_BROKER_URL = 'redis://:as8qO8oBJBLiOCVNuCr52YNLKNHttUah@redis-11841.c251.east-us-mz.azure.cloud.redislabs.com:11841/'
+# CELERY_BROKER_URL = 'redis://:as8qO8oBJBLiOCVNuCr52YNLKNHttUah@redis-11841.c251.east-us-mz.azure.cloud.redislabs.com:11841/'
 #CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
 
 CELERY_TASK_SELERLIZER = 'json'
--- websocketManage/consumers.py
+++ websocketManage/consumers.py
@@ -1,6 +1,5 @@
 import json
 
-from channels.exceptions import StopConsumer
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
@@ -11,8 +10,8 @@ from rassperypiInfo.models import RassperySystem
 
 class openDoorCunsumer(WebsocketConsumer):
     def connect(self):
-        self.succss_connect=False
-
+        self.check_level=False
+        self.accept()
         try:
             self.token=str(dict(self.scope['headers'])[b'token'])
             self.token=self.token[2:len(self.token)-1]
@@ -21,44 +20,45 @@ class openDoorCunsumer(WebsocketConsumer):
         except:
             self.send("not serial-rasperypi &| token")
             self.disconnect(close_code=1)
-        try:
-            rassperypiInfo=RassperySystem.objects.get(serial_rasperyPi=self.serial_rasperyPi , token_connect_rassperypi=self.token)
-            rassperypiInfo.online_status=1
-            rassperypiInfo.save()
-            self.rassperypiInfo=rassperypiInfo
-        except:
-            self.send("not rassperyPi with informathons")
-            self.disconnect(close_code=1)
-        self.rassperypiInfo.online_status=1
-        self.rassperypiInfo.save()
-        self.group_name=f"doorSecurity_{self.serial_rasperyPi}"
-        async_to_sync(self.channel_layer.group_add)(
-            self.group_name,
-            self.channel_name
-        )
-        self.succss_conect = True
+        else:
+            try:
+                rassperypiInfo=RassperySystem.objects.get(serial_rasperyPi=self.serial_rasperyPi , token_connect_rassperypi=self.token)
+                rassperypiInfo.online_status=1
+                rassperypiInfo.save()
+                self.rassperypiInfo=rassperypiInfo
+            except:
+                self.send("not rassperyPi with informathons")
+                self.disconnect(close_code=1)
+            else:
+                self.rassperypiInfo.online_status=1
+                self.rassperypiInfo.save()
+                self.group_name=f"doorSecurity_{self.serial_rasperyPi}"
+                async_to_sync(self.channel_layer.group_add)(
+                    self.group_name,
+                    self.channel_name
+                )
+                self.check_level = True
 
-        try:
-            informationService=InformationService.objects.get(rassperypiInfo=self.rassperypiInfo)
-            self.send(json.dumps({'massege': 'change status openDoor', 'code': (1011 + informationService.status_opendoor)}))
-        except:
-            self.send("not rassperyPi with informathons")
-            self.disconnect(close_code=1)
-        self.accept()
+                try:
+                    informationService=InformationService.objects.get(rassperypiInfo=self.rassperypiInfo)
+                    self.send(json.dumps({'massege': 'change status openDoor', 'code': (1011 + informationService.status_opendoor)}))
+                except:
+                    self.send("not rassperyPi with informathons")
+                    self.disconnect(close_code=1)
 
     def disconnect(self, close_code):
-        if self.succss_conect:
+        if self.check_level:
             self.rassperypiInfo.online_status=2
             self.rassperypiInfo.save()
             self.channel_layer.group_discard(
                 self.group_name,
                 self.channel_layer
             )
-        self.close()
+        else:
+            self.close()
 
     def receive(self, text_data=None, bytes_data=None):
         if text_data:
-
             self.group_name = f"doorSecurity_{self.token}"
             channel_layer = get_channel_layer()
             async_to_sync(channel_layer.group_send)(
