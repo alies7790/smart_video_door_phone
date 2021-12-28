580086ff944839989b0b62c97632666e27aab88e
alies7790 <ali.e7790@gmail.com>
2021-12-28 19:04:37 +0330

    resize Image

--- doorSecurity/resize_image.py
+++ doorSecurity/resize_image.py
@@ -2,13 +2,19 @@
 import base64
 from PIL import Image
 from io import BytesIO
+from rest_framework.response import Response
+from rest_framework import  status
 def resizeImage(picture):
-    img = Image.open(base64.decodestring(picture))
+    try:
+        img = Image.open(BytesIO(base64.b64decode(picture)))
+    except:
+        return Response({"message": "not base64"},
+                                status=status.HTTP_400_BAD_REQUEST)
     basewidth = 300
     wpercent = (basewidth / float(img.size[0]))
     hsize = int((float(img.size[1]) * float(wpercent)))
     img = img.resize((basewidth, hsize), Image.ANTIALIAS)
     buffered = BytesIO()
     img.save(buffered, format="JPEG")
-    resize_picture = base64.encodestring(buffered.getvalue())
+    resize_picture = base64.b64encode(buffered.getvalue())
     return resize_picture
