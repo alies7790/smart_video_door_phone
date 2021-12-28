e62a642637c91991849436856a9a9e327b06508f
alies7790 <ali.e7790@gmail.com>
2021-12-28 18:58:04 +0330

    resize Image token authoraize delete ago token when login new device jwt for steps login and forget

--- doorSecurity/resize_image.py
+++ doorSecurity/resize_image.py
@@ -3,12 +3,12 @@ import base64
 from PIL import Image
 from io import BytesIO
 def resizeImage(picture):
-    img = Image.open(BytesIO(base64.b64decode(picture)))
-    basewidth = 400
+    img = Image.open(base64.decodestring(picture))
+    basewidth = 300
     wpercent = (basewidth / float(img.size[0]))
     hsize = int((float(img.size[1]) * float(wpercent)))
     img = img.resize((basewidth, hsize), Image.ANTIALIAS)
     buffered = BytesIO()
     img.save(buffered, format="JPEG")
-    resize_picture = base64.b64encode(buffered.getvalue())
+    resize_picture = base64.encodestring(buffered.getvalue())
     return resize_picture
